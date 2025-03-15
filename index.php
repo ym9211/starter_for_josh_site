8c9685a5f1675ba7deb36b31297ce707dec9e471
ym9211 <ddjugh@gmail.com>
2025-03-15 15:13:07 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php get_header();?>
+
+        <section class="container-fluid aboutbg text-center">
+            <div class="container">
+    <h2>GAMES &amp; LEVEL DESIGNER</h2>
+    <P class = "particle">...and particle wizard</P>
+    <p class = "about-text"> My name is josh Whitkin. I'm a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects. What makes me passionate about Games is the combination of technology and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments.
+        I always tell a story.</p>
+    </div>
+    <div class="blocker"></div>
+    </section>
+
+        <section class="container-fluid articlebg">
+            <div class="container">
+                <h3>LATEST ARTICLES</h3>
+                <div class="raw">
+
+    <div class="col-md-4 text-center">
+    <img class="img-fluid" alt="portrait" src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port1.jpg">
+    <h4 class="article-title">A PORTRAIT</h4>
+    <p class="date">12 December 2020</p>
+    <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and interview in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. 
+    </p>
+    <a class= "readmore" href="#">CONTINUE READING</a>
+    </div>
+               
+    <div class="col-md-4 text-center">
+    <img class="img-fluid" alt="portrait" src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port2.jpg">
+    <h4 class="article-title">A MODEL</h4>
+    <p class="date">12 December 2020</p>
+    <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS schlars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+    </p>
+    <a class="readmore" href="#">CONTINUE READING</a>
+    </div>
+                
+    <div class="col-md-4 text-center">
+    <img class="img-fluid" alt="portrait" src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port3.jpg">
+    <h4 class="article-title">A GAMES DESIGN</h4>
+    <p class="date">12 December 2020</p>
+    <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS schlars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+    </p>
+    <a class="readmore" href="#">CONTINUE READING</a>
+    </div>
+
+    </div><!--row-->
+    </div><!--container-->
+    </section><!--container-fluid-->
+<?php get_footer();?>
+
